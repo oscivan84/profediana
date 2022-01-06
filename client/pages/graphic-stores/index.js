7a0adb0bdde09e3e32fb2bd911ac7eac1a0c3a2f
wveimar.mamian@gmail.com <wveimar.mamian@gmail.com>
2022-01-06 14:50:21 -0500

    llamado api

--- client/pages/graphic-stores/index.js
+++ client/pages/graphic-stores/index.js
@@ -4,60 +4,69 @@ import axios from "axios";
 import { useState,useEffect } from 'react';
 import { FormGroup,Label,Input ,Button,Container,Row, Col} from 'reactstrap'
 
+const data = {
+  labels: ['Cede1', 'Cede2', 'Cede3', 'Cede4', 'Cede5', 'Cede6'],
+  datasets:[{
+      label: 'Total ventas por cedes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 2
+  }]
+};
 
-
+const opciones={
+  maintainAspectRatio:false,
+  responsive: true
+};
 const GraphicStores = () => {
-  
-   const data = {
-        labels: ['Cede1', 'Cede2', 'Cede3', 'Cede4', 'Cede5', 'Cede6'],
-        datasets:[{
-            label: 'Total ventas por cedes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)"
-              ],
-              borderWidth: 2
-        }]
-    }
-    const opciones={
-        maintainAspectRatio:false,
-        responsive: true
-    };
+  const dataSales=[
+    {id:1, name:"cede1",sales:200},
+    {id:2, name:"cede2",sales:250},
+    {id:3 ,name:"cede3",sales:300},
+    {id:4, name:"cede4",sales:500},
+    {id:5, name:"cede5",sales:600},
+    {id:6, name:"cede6",sales:521}
+  ]
+
+
+const [sales,setSales] = useState (null)
+  useEffect(() => {
+    setSales(dataSales)
+    console.log(sales)
+   peticionApi();
+},[])
+
 
     const peticionApi =async()=>{
-        await axios.get("https://pokeapi.co/api/v2/ability/4")
-        .then(response=>{
-            console.log(response.data)
-        })
-
-        useEffect(() => {
-            console.log("hh")
-           peticionApi();
-        },[])
+       const dataAPI = await fetch("https://pokeapi.co/api/v2/ability/4")
+       const dataRes = await dataAPI.json()
+        console.log(dataRes)
     }
     return (
         <Container style={{display: 'flex', flexDirection: 'column'}} className="p-4">
             <Row xs = "2">
                 <Col>
             <FormGroup>
-    <Label for="exampleDate">
+    <Label for="fechaInicial">
       Fecha Inicial
     </Label>
     <Input
-      id="exampleDate"
+      id="fechaInicial"
       name="date"
       placeholder="date placeholder"
       type="date"
@@ -66,11 +75,11 @@ const GraphicStores = () => {
                 </Col>
                 <Col>
   <FormGroup>
-    <Label for="exampleDate">
+    <Label for="fechaFinal">
       Fecha Final
     </Label>
     <Input
-      id="exampleDate"
+      id="fechaFinal"
       name="date"
       placeholder="date placeholder"
       type="date"
@@ -84,55 +93,9 @@ const GraphicStores = () => {
   >
     Click Me
   </Button>
-             <Pie data={data} option={opciones} style={{width: '50%' ,height: '50%'}}/>;
+             <Pie data={data} option={opciones} style={{width: '50%' ,height: '50%'}} onClick={peticionApi}/>;
         </Container>
     )
 }
 
 export default GraphicStores
-
-
-
-
-
-
-
-
-// import React from 'react';
-// //import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-// import { Pie } from 'react-chartjs-2';
-
-// //ChartJS.register(ArcElement, Tooltip, Legend);
-
-
-
-// export function GraphicStores() {
-//     export const data = {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [
-//           {
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//               'rgba(255, 99, 132, 0.2)',
-//               'rgba(54, 162, 235, 0.2)',
-//               'rgba(255, 206, 86, 0.2)',
-//               'rgba(75, 192, 192, 0.2)',
-//               'rgba(153, 102, 255, 0.2)',
-//               'rgba(255, 159, 64, 0.2)',
-//             ],
-//             borderColor: [
-//               'rgba(255, 99, 132, 1)',
-//               'rgba(54, 162, 235, 1)',
-//               'rgba(255, 206, 86, 1)',
-//               'rgba(75, 192, 192, 1)',
-//               'rgba(153, 102, 255, 1)',
-//               'rgba(255, 159, 64, 1)',
-//             ],
-//             borderWidth: 1,
-//           },
-//         ],
-//       };
-//   return <Pie data={data} />;
-// }
-
